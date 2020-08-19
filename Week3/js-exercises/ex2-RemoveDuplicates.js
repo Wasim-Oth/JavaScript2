daf55eaa18401e929d7b656ffb71f8e857235814
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-08-19 13:59:47 +0200

    added ex2-RemoveDuplicates.js

--- Week3/js-exercises/ex2-RemoveDuplicates.js
+++ Week3/js-exercises/ex2-RemoveDuplicates.js
@@ -12,10 +12,27 @@ does not return anything but removes any duplicate elements from the array.
 
 
 // WRITE YOUR FUNCTION HERE
+function removeDuplicates(letter){
+  // let ff = letter.sort();
+  // console.log(ff);
+  let newLetters=[];
+  letter.forEach((element, ind) => {
+    //  console.log('lertters are: ' + element + '; '+ 'inedx is: ' + ind)
+    //  console.log(letter.indexOf(element));
+    if ( letter.indexOf(element) == ind){
+      // console.log(element);
+      newLetters.push(element)
+    }
+  });
+  return newLetters
+}
 
 const letters = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c', 'b'];
 
-removeDuplicates(letter);
+// console.log(removeDuplicates(letters));
+const removeDuplicate = (removeDuplicates(letters));
 
-if (letters === ['a', 'b', 'c', 'd', 'e', 'f'])
+// i can compare two arrays in this 2 ways
+// if (JSON.stringify(removeDuplicate) ===JSON.stringify(['a', 'b', 'c', 'd', 'e', 'f']))
+if (removeDuplicate == ['a', 'b', 'c', 'd', 'e', 'f'] +[])
   console.log("Hooray!")
