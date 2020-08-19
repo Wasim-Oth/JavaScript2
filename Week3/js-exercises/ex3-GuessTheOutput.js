eca1f8ab1ab96f748d63c2d61e1ce71ade16dca6
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-08-19 14:09:46 +0200

    added ex3-GuessTheOutput.js

--- Week3/js-exercises/ex3-GuessTheOutput.js
+++ Week3/js-exercises/ex3-GuessTheOutput.js
@@ -19,3 +19,34 @@ const x = (function () {
 })();
 
 x();
+
+
+
+/* it will alert 12 because inside the outer function'x' we are reassigning
+the global variable 'a' to 12 and then we are returning an inner function which 
+alerting the variable 'a'. so it will look for the variable 'a' inside
+the inner function will not find it, so it will look in the outer function it find a = 12
+ */
+
+  /*for me the parentheses here })(); are confusing. why sometime we call the function
+   only with the name without parentheses and sometime we have to use them?
+   i still can't understaand that i always have to try it*/
+
+
+   //in this example if i add parentheses to 'return insideFuncyion' i get error!
+
+  //  function createBase(num1) {
+  //   // Put here your logic...
+  //   let addnum;
+  //   const insideFunction = (num2)=>{
+  //     addnum = num1 + num2;
+  //     return addnum
+  //   }
+  //   return insideFunction;
+  // };
+  
+  // const addSix = createBase(6);
+  
+  // console.log(addSix(9));
+  // console.log(addSix(18));
+  // console.log(addSix(30));
