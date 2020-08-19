b1af68742d184e0b599be789c421bf4ab4aa172f
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-08-19 14:10:15 +0200

    added ex4-GuessMore.js

--- Week3/js-exercises/ex4-GuessMore.js
+++ Week3/js-exercises/ex4-GuessMore.js
@@ -12,10 +12,14 @@ const x = 9;
 
 function f1(val) {
   val = val + 1;
-  return val;
+  console.log(val);
 }
 f1(x);
 console.log(x);
+
+// it should throw an error becuase its Assignment to constant variable.
+// but i can see in the log the resault is 9. it's working only as a parameter otherwise
+// if i delete the parameter and replace the val with the variale name then it's throwing the error!
 
 const y = {
   x: 9
@@ -23,7 +27,12 @@ const y = {
 
 function f2(val) {
   val.x = val.x + 1;
-  return val;
+  return y;
 }
 f2(y);
 console.log(y);
+
+
+
+//the output shoild be {x:10} becuase we are reassigning the property 'x' in the
+//  object 'y' 
